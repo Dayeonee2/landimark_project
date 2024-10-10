e8ecbf8aee4ad8e849cc1387f3503a88b687f85d
Ji-Han-Lee <rjsgml3747@naver.com>
2024-10-10 09:24:31 +0900

    jihanchatbot delete

--- jihan/static/page.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // "챗봇 시작하기" 버튼 클릭 시 챗봇 페이지로 이동
-    document.querySelector('.start-chatbot').addEventListener('click', function () {
-        window.location.href = '/chatbot/';
-    });
-});
